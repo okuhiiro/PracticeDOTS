bdef69322295cc130f6b52e6873604931918f3b6
okuhiiro <okuhiiro@gmail.com>
2024-12-12 22:54:53 +0900

    ManyCollisionSystemの再実装

--- Assets/2.ManyCollision/ManyCollisionSystem.cs
+++ Assets/2.ManyCollision/ManyCollisionSystem.cs
@@ -1,5 +1,7 @@
 using Unity.Burst;
+using Unity.Collections;
 using Unity.Entities;
+using Unity.Jobs;
 using Unity.Mathematics;
 using Unity.Transforms;
 
@@ -8,51 +10,159 @@ using Unity.Transforms;
 partial struct ManyCollisionSystem : ISystem
 {
     private const float Diameter = 0.2f;
+    private const float Radius = 0.1f;
+    
+    NativeList<Entity> m_entities;
+    NativeList<LocalTransform> m_Transforms;
 
     [BurstCompile]
     public void OnCreate(ref SystemState state)
     {
         state.RequireForUpdate<ManyCollisionData>();
         state.RequireForUpdate<ManyCollision>();
+        
+        m_entities = new NativeList<Entity>(1000, Allocator.Persistent);
+        m_Transforms = new NativeList<LocalTransform>(1000, Allocator.Persistent);
     }
 
     [BurstCompile]
     public void OnUpdate(ref SystemState state)
     {
         NoJobCollision(ref state);
+        //JobCollision(ref state);
+    }
+    
+        
+    [BurstCompile]
+    public void OnDestroy(ref SystemState state)
+    {
+        m_entities.Dispose();
+        m_Transforms.Dispose();
     }
 
     [BurstCompile]
     private void NoJobCollision(ref SystemState state)
     {
-        foreach (var (transform1, entity1) in
+        foreach (var (transform, entity) in
                  SystemAPI.Query<RefRW<LocalTransform>>().WithEntityAccess())
         {
-            foreach (var (transform2, entity2) in
+            float2 Displacement = float2.zero;
+            uint Weight = 0;
+            
+            foreach (var (otherTransform, otherEntity) in
                      SystemAPI.Query<RefRW<LocalTransform>>().WithEntityAccess())
             {
-                if (entity2.Index == entity1.Index)
+                float2 towards = transform.ValueRO.Position.xy - otherTransform.ValueRO.Position.xy;
+                
+                float distancesq = math.lengthsq(towards);
+                float radiusSum = Radius + Radius;
+                if (distancesq > radiusSum * radiusSum || entity == otherEntity)
                     continue;
-
-                float3 vec = transform2.ValueRO.Position - transform1.ValueRO.Position;
-
-                float3 dir;
-                if (vec.Equals(float3.zero))
-                {
-                    dir = new float3(0, 1, 0);
-                }
-                else
-                {
-                    dir = math.normalizesafe(vec);
-                }
-                float distance = math.sqrt(math.lengthsq(vec));
-
-                if (distance < Diameter)
-                {
-                    transform1.ValueRW.Position -= (Diameter - distance) * dir / 2;
-                    transform2.ValueRW.Position += (Diameter - distance) * dir / 2;
-                }
+                
+                float distance = math.sqrt(distancesq);
+                float penetration = radiusSum - distance;
+                penetration = (penetration / distance);
+                
+                Displacement += towards * penetration;
+                Weight++;
             }
+            
+            if (Weight > 0)
+            {
+                Displacement = Displacement / Weight;
+                transform.ValueRW.Position += new float3(Displacement, 0);
+            }
+        }
+    }
+    
+    [BurstCompile]
+    private void JobCollision(ref SystemState state)
+    {
+        m_entities.Clear();
+        m_Transforms.Clear();
+        
+        CopyJob job1 = new CopyJob
+        {
+            Entities = m_entities,
+            Transforms = m_Transforms,
+        };
+        JobHandle job1Handle = job1.Schedule(state.Dependency);
+        job1Handle.Complete();
+        
+        HashCollisionJob job2 = new HashCollisionJob
+        {
+            Entities = m_entities,
+            Transforms = m_Transforms.AsArray()
+        };
+        state.Dependency = job2.ScheduleParallel(state.Dependency);
+    }
+    
+    [BurstCompile]
+    partial struct CopyJob : IJobEntity
+    {
+        public NativeList<Entity> Entities;
+        public NativeList<LocalTransform> Transforms;
+
+        void Execute(in Entity entity, in LocalTransform transform)
+        {
+            Entities.Add(entity);
+            Transforms.Add(transform);
+        }
+    }
+    
+    [BurstCompile]
+    partial struct HashCollisionJob : IJobEntity
+    {
+        [ReadOnly]
+        public NativeList<Entity> Entities;
+        [ReadOnly]
+        public NativeArray<LocalTransform> Transforms;
+        
+        void Execute(in Entity entity, ref LocalTransform transform)
+        {
+            var action = new MyCollision
+            {
+                Entity = entity,
+                Transform = transform,
+            };
+            var length = Entities.Length;
+            for (int i = 0; i < length; i++)
+            {
+                action.Execute(Entities[i], Transforms[i]);
+            }
+            if (action.Weight > 0)
+            {
+                action.Displacement = action.Displacement / action.Weight;
+                transform.Position += new float3(action.Displacement, 0);
+            }
+        }
+    }
+    
+    struct MyCollision
+    {
+        [ReadOnly]
+        public Entity Entity;
+        [ReadOnly]
+        public LocalTransform Transform;
+        
+        public float2 Displacement;
+        public uint Weight;
+
+        public void Execute(in Entity otherEntity, in LocalTransform otherTransform)
+        {
+            float2 towards = Transform.Position.xy - otherTransform.Position.xy;
+            
+            float distancesq = math.lengthsq(towards);
+            float radiusSum = Radius + Radius;
+            if (distancesq > radiusSum * radiusSum || Entity == otherEntity)
+                return;
+            
+            float distance = math.sqrt(distancesq);
+            float penetration = radiusSum - distance;
+            penetration = (penetration / distance);
+            
+            Displacement += towards * penetration;
+            Weight++;
         }
     }
 }
